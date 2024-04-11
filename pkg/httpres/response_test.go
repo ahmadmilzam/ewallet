d19899246a48ad716646a0d96d861b9e653f87bc
Ahmad Milzam <ahmad.milzam@flip.id>
2024-04-12 02:43:43 +0700

    Fix response pkg test

--- pkg/httpres/response_test.go
+++ pkg/httpres/response_test.go
@@ -1,6 +1,9 @@
 package httpres
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,25 +48,28 @@ func TestGenerateOK(t *testing.T) {
 	assert.Equal(t, expectedArr, actualArr)
 }
 
-// func TestGenerateErr(t *testing.T) {
-// 	errMessage := "Bad Request"
-// 	actual := GenerateErr(GenericBadRequest, errMessage)
-// 	expected := HttpResponse{
-// 		Success: false,
-// 		Data:    nil,
-// 		Error:   ErrorDetails{Code: GenericBadRequest, Message: errMessage},
-// 	}
-// 	// fe.InitServiceCode(issuer.TransactionManagementService)
-// 	assert.Equal(t, expected, actual)
-// }
+func TestGenerateErr(t *testing.T) {
+	errMessage := "Bad Request"
+	err := fmt.Errorf("%s_%w", GenericBadRequest, errors.New("BAD_REQUEST"))
+	actual := GenerateErrResponse(err, errMessage)
+	expected := HttpResponse{
+		Success: false,
+		Error: &ErrorDetails{
+			Code:    GetCaseCode(err),
+			Message: errMessage,
+		},
+	}
+	// fe.InitServiceCode(issuer.TransactionManagementService)
+	assert.Equal(t, expected, actual)
+}
 
-// func TestGetStatusCode(t *testing.T) {
-// 	assert.Equal(t, http.StatusBadRequest, GetErrStatusCode(GenericBadRequest))
-// 	assert.Equal(t, http.StatusUnauthorized, GetErrStatusCode(GenericUnauthorized))
-// 	assert.Equal(t, http.StatusNotFound, GetErrStatusCode(GenericNotFound))
-// 	assert.Equal(t, http.StatusMethodNotAllowed, GetErrStatusCode(GenericMethodNotAllowed))
-// 	assert.Equal(t, http.StatusConflict, GetErrStatusCode(GenericConflict))
-// 	assert.Equal(t, http.StatusUnprocessableEntity, GetErrStatusCode(GenericUnprocessable))
-// 	assert.Equal(t, http.StatusTooManyRequests, GetErrStatusCode(GenericTooManyRequests))
-// 	assert.Equal(t, http.StatusInternalServerError, GetErrStatusCode(GenericInternalError))
-// }
+func TestGetStatusCode(t *testing.T) {
+	assert.Equal(t, http.StatusBadRequest, GetStatusCode(errors.New(GenericBadRequest)))
+	assert.Equal(t, http.StatusUnauthorized, GetStatusCode(errors.New(GenericUnauthorized)))
+	assert.Equal(t, http.StatusNotFound, GetStatusCode(errors.New(GenericNotFound)))
+	assert.Equal(t, http.StatusMethodNotAllowed, GetStatusCode(errors.New(GenericMethodNotAllowed)))
+	assert.Equal(t, http.StatusConflict, GetStatusCode(errors.New(GenericConflict)))
+	assert.Equal(t, http.StatusUnprocessableEntity, GetStatusCode(errors.New(GenericUnprocessable)))
+	assert.Equal(t, http.StatusTooManyRequests, GetStatusCode(errors.New(GenericTooManyRequests)))
+	assert.Equal(t, http.StatusInternalServerError, GetStatusCode(errors.New(GenericInternalError)))
+}
